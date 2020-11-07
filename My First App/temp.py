8831659b0fde6a8a9251a1d9bae437304ae08c2c
Amir P <amirprezaei@gmail.com>
2020-11-07 21:59:59 +0000

    init test 2

--- My First App/temp.py
+++ My First App/temp.py
@@ -20,4 +20,4 @@ elif high_low_choice == 'high' and random_num in range((max_num // 2) + 1, max_n
 else:
     print('You guessed incorrectly, AI plays first.\n')
 
-# this is added to test github sync settings
+# this is added to test github sync settings 
